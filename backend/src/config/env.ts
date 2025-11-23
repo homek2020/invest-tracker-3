44fdcea52c626a4a4f38912dba603ebb77ee177e
Valik <homek.ru@gmail.com>
2025-11-23 12:58:30 +0300

    Config idea

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -4,7 +4,9 @@ dotenv.config();
 
 export const env = {
   port: Number(process.env.PORT ?? 4000),
-  mongoUri: process.env.MONGO_URI ?? 'mongodb://localhost:27017/invest-tracker',
+  mongoUri: process.env.MONGO_URI ?? 'mongodb+srv://homekru_db_user:cTyCZiBjUP8wUuFc@cluster0.sbyoqte.mongodb.net/?appName=Cluster0',
   jwtSecret: process.env.JWT_SECRET ?? 'dev-secret',
   initToken: process.env.INIT_TOKEN,
 };
+//homekru_db_user
+//cTyCZiBjUP8wUuFc
